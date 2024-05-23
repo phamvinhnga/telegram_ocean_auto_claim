1b06857db64ea6926e371e6796c734f151667173
nga.vinhpham <40154445+phamvinhnga@users.noreply.github.com>
2024-05-23 21:59:36 +0700

    Update content.js

--- content.js
+++ content.js
@@ -28,9 +28,11 @@ function process() {
   try {
     console.log(processingTurn);
     // Mở menu
+    console.log("Mở menu");
     clickElementBySelector("#section-home > div > header > button.relative.p-4");
     setTimeout(() => {
       // Chon user
+      console.log("Chon user");
       let hasUser = clickElementBySelector(accounts[processingTurn]);
       if (!hasUser) {
         processingTurn = accountCount;
@@ -39,10 +41,12 @@ function process() {
       }
       setTimeout(() => {
         // Nút Claim ở ngoài của ví để vào Ocean Game
+        console.log("Nút Claim ở ngoài của ví để vào Ocean Game");
         clickElementBySelector("#section-home > div > div > div.swiper.swiper-initialized.swiper-horizontal.mySwiper > div.swiper-wrapper > div.swiper-slide.swiper-slide-active > div > div.item-1 > div._item-1_2 > div.ml-auto.mt-3 > button");
 
         setTimeout(() => {
           // Nút Claim OCEAN ở trong Ocean Game
+          console.log("Nút Claim OCEAN ở trong Ocean Game");
           let claimButton = clickElementBySelector("#section-transaction > div.block-data.h-full > div > div.overlay.relative > div > div > div > button");
           if (claimButton) {
             nextAccount(15000);
